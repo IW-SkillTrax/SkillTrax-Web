f8f2b1f09aa907cd7d5cd64ef562fd1550594ce2
Brad Cutts <brad.cutts@infoworks-tn.com>
2018-07-20 16:10:30 -0500

    made navbar

--- src/app/employee/employee.routes.ts
+++ src/app/employee/employee.routes.ts
@@ -3,5 +3,6 @@ import { ProfileComponent } from './profile/profile.component';
 
 export const employeeRoutes = [
     {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
     {path: 'profile', component: ProfileComponent}
 ]
